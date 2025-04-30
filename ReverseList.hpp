f2a4ce04d7054d16c71563c5ac8f21863b70e009
Jack Penn <69122588+Jack-Penn@users.noreply.github.com>
2025-04-30 00:22:44 +0000

    Question 2 initial commit

--- ReverseList.hpp
+++ ReverseList.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 
 struct Node {
     int data;
@@ -16,6 +17,17 @@ public:
     
     LinkedList reverseList() const {
         // implement your function here!
+        std::vector<int> nodeData;
+        Node* temp = head;
+        while(temp != nullptr) {
+            nodeData.push_back(temp->data);
+            temp = temp -> next;
+        }
+        LinkedList result;
+        for(int i = nodeData.size()-1; i >= 0; i--) {
+            result.addHead(nodeData.at(i));
+        }
+        return result;
     }
 
     Node* head = nullptr;
